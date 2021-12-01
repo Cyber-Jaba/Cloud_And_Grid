7eb977a593e7cbf979bc1d398ca7c09083038784
KOT98 <tiger1998sasha@gmail.com>
2021-12-01 17:40:41 +0200

    draft session test

---
+++ P1cture/Image/ExtendMagic/Models/Session.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using P1cture.Image.JSON;
+
+namespace P1cture.Image.ExtendMagic
+{
+    public partial class Session
+    {
+        public int Text { get; set; }
+        public PictureGeneartionStage Stage { get; set; }
+    }
+
+    public enum PictureGeneartionStage
+    {
+        Text, Processing, Result
+    }
+}
